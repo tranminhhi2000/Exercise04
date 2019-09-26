1100c0e4e5dc0643a5fe366ef5b08545833a05c9
tranminhhi2000 <tranminhhi2732000@gmail.com>
2019-09-26 18:01:21 +0700

    Ex04

--- Application2/ConsoleApplication1/Program.cs
+++ Application2/ConsoleApplication1/Program.cs
@@ -6,11 +6,28 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Alo Alo");
+        }
+
+        //public static bool IsOdd(int n)
+        //{
+        //    if (n % 2 != 0)
+        //    {
+        //        return true;
+        //    }
+        //    return false;
+        //}
+        public static bool IsEven(int n)
+        {
+            if (n % 2 == 0)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
---
+++ Application2/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        //public void TestIsOdd()
+        //{
+        //    bool result3 = Program.IsOdd(3);
+        //    Assert.AreEqual(true, result3);
+
+        //    bool result4 = Program.IsOdd(4);
+        //    Assert.AreEqual(false, result4);
+        //}
+
+        public void TestIsEven()
+        {
+            bool result5 = Program.IsEven(5);
+            Assert.AreEqual(false, result5);
+
+            bool result6 = Program.IsEven(6);
+            Assert.AreEqual(true, result6);
+        }
+    }
+}
